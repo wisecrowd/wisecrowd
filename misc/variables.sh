64b0e1a228ec41eb4b3fab190f75184c2d9305b8
RebeccaFransson <rebecca@kollektiva.se>
2018-04-05 09:18:27 +0200

    misc

---
+++ misc/variables.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Variables used by terminal m command to compile/build SCSS to CSS
+SCSS_DIRECTORY_NAME="modules/web/site/src/main/resources/websites/site/cdn/scss"
+SCSS_FILE_NAME="modules/web/site/src/main/resources/websites/site/cdn/scss/style.scss"
+CSS_FILE_NAME="modules/web/site/src/main/resources/websites/site/cdn/scss/style.css"
+
+# Variable used by terminal m command for browser sync
+BROWSER_SYNC_DIRECTORY_NAME="modules/web/site/target/classes/websites/site"
